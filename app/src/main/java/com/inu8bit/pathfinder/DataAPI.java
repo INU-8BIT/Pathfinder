39d4b7bf6ab1dc84a71776f6b6ed29f0c91182d7
Junsu Kim <fenslett@gmail.com>
2018-05-04 01:09:46 +0900

    Changed DataAPI

--- app/src/main/java/com/inu8bit/pathfinder/DataAPI.java
+++ app/src/main/java/com/inu8bit/pathfinder/DataAPI.java
@@ -18,28 +18,55 @@ import static com.inu8bit.pathfinder.BuildConfig.DataAPIKey;
  *      1. Get the list of Nearby bus stop
  */
 public class DataAPI extends APIWrapper {
-    DataAPI(){
-        serviceKey = DataAPIKey;
-        url = new StringBuilder("http://openapi.tago.go.kr/openapi/service/BusSttnInfoInqireService/getCrdntPrxmtSttnList");
+
+    public static class BusStop {
+        public String nodeNo;
+        public String nodeNm;
+        public String nodeId;
+        public String cityCode;
+
+        BusStop(String nodeId, String nodeNm, String nodeNo, String citycode){
+            this.nodeId = nodeId;
+            this.nodeNm = nodeNm;
+            this.nodeNo = nodeNo;
+            this.cityCode = citycode;
+        }
     }
 
-    public List<String> getNearbyBusStop(double lat, double lon) throws InterruptedException, ExecutionException, JSONException{
+    private String GET_NEARBY_BUS_STOP = "BusSttnInfoInqireService/getCrdntPrxmtSttnList";
+    private String GET_BUS_ARRIVAL_INFO = "ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList";
+    private String GET_BUS_SPECIFIC_ARRIVAL_INFO = "ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList";
+
+    DataAPI(){
+        serviceKey = DataAPIKey;
+        url = new StringBuilder("http://openapi.tago.go.kr/openapi/service/");
+    }
+
+    public Map<Integer, String[]> getNearbyBusStop(double lat, double lon) throws InterruptedException, ExecutionException, JSONException{
+        url.append(GET_NEARBY_BUS_STOP);
         params.put("ServiceKey", this.serviceKey);
         params.put("gpsLati", String.valueOf(lat));
         params.put("gpsLong", String.valueOf(lon));
         params.put("_type", "json");
 
         this.method = "GET";
-        return this.getLists(this.execute().get());
+        return this.parseList(this.execute().get());
     }
 
-    private List<String> getLists(JSONObject _obj) throws JSONException {
-        // TODO: make these following steps as Class or structural object.
-        JSONArray busStopList = _obj.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < busStopList.length() && i < 5; i++) {
-            list.add(busStopList.getJSONObject(i).getString("nodenm") + " " + busStopList.getJSONObject(i).getInt("nodeno"));
+    private Map<Integer, String[]> parseList(JSONObject jsonObject) throws JSONException {
+        JSONArray busStopList = jsonObject.getJSONObject("response")
+                .getJSONObject("body")
+                .getJSONObject("items").getJSONArray("item");
+
+        Map<Integer, String[]> busList = new HashMap<>();
+        for (int i = 0; i < busStopList.length() && i < 3; i++) {
+            JSONObject elem = busStopList.getJSONObject(i);
+            busList.put(elem.getInt("nodeno"), new String [] {
+                    Integer.toString(elem.getInt("citycode")),
+                    elem.getString("nodenm"),
+                    elem.getString("nodeid")
+            });
         }
-        return list;
+        return busList;
     }
 }
